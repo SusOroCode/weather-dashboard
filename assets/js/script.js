5541017ad41d44face47e8439ac0120a75405b17
Tanuki <susann.orosco33@gmail.com>
2023-03-30 22:26:13 -0700

    worked on fixing the broken api Link, supporting teacher helped in office hours, wrote logic to display html items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,43 @@
-var apiUrlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`;
-var apiUrlForecast = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+//var apiUrlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`;
+//var apiUrlForecast = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
 
-var apiKey = '43dcd3f9c299335aeb92b0cf181183cc';
+//GLOBAL VARIABLES
 
-var weatherFormEl = document.querySelector('#weather-city-form');
+var searchFormEl = document.querySelector('#weather-search-form');
 var enterCityEl = document.querySelector('#enter-city');
 var submitButtenEl = document.querySelector('#submit');
+var cityNameEl = document.querySelector('#city-name');
+var pastSearchEl = document.querySelector('#past-search');
+var todayWeatherEl = document.querySelector('#today-weather');
+var fiveDayForecastEl = document.querySelector('#forecast-five-day');
+var city;
+var APIKey = '91d4837977f02f96f0a5781cb7dbd37d';
 
 function cityFormSubmit(event) {
     event.preventDefault();
 
     var formInputValue = enterCityEl.value;
-    console.log('user input: ' + formInputValue);
+    displayWeather(formInputValue);
 
-    var apiUrlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`
-}   
+}
+
+    function displayWeather(formInputValue) {
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?appid=${APIKey}&q=${formInputValue}&units=imperial`;
+    
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data); // It works! :) YAY
+
+        });
+};
+
+function renderResponse(userData) {
+    document.querySelector
+}
+
+//Add event listener to form (type submit)
+
+document.getElementById('button').addEventListener('click', cityFormSubmit);
